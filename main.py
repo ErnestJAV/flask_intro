757d66a6f5920a3bc1a12096cabe4a630e621c6c
ErnestJav <ernest.javmen@gmail.com>
2022-11-15 20:07:05 +0200

    flask intro

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 """
 -flask
--path/query parameter
+-path parameter
+-query parameter
 -GET/POST methods
+-forms
 -render_template
 -extend template
 """
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])  # decorator
+def hello_world():
+    print(request.method)
+    print(request.form.get('username'))
+    return render_template('forms.html')
+
+app.run()
 
